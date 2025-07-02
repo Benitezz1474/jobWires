3b032ba07f9ba7389c05797cc0807b76e5c25d87
fernando 3ro MN nocturno <benitezz1474@outlook.com>
2025-07-02 23:01:58 +0700

    version personal(Benitez V1): se modifica index,registro y login

--- sessionVerify.js
+++ sessionVerify.js
@@ -1,18 +1,18 @@
 export const sessionVerify=()=>{ //verifica que exista una sesion con cedula
 
-    const url_registro = "http://localhost/PROYECTO/REGISTRO/register.html";
-    const url_login = "http://localhost/PROYECTO/LOGIN/login.html";
+    const url_registro = "http://localhost/PROYECTO/REGISTRO/index.html";
+    const url_login = "http://localhost/PROYECTO/LOGIN/index.html";
     const url_actually = window.location.href;
 // const url_finally = "http://localhost/PROYECTO/CONTENT/index.html";
 
 //si no existe la sesion debe registrarse: lo madno a que se registre
-if(!(sessionStorage.getItem("CI")) && (url_actually != url_registro)){
+if(!(sessionStorage.getItem("email")) && (url_actually != url_registro  && url_actually != url_login)){
     
-    window.location.href = url_registro; //lo mando al registro
+    // window.location.href = url_registro; //lo mando al registro
 }
 
 //si no tiene sesion PERO se encuentra en el login o registro no pasa nada, no lo mando a ningun lado
-else if (!(sessionStorage.getItem("CI")) && 
+else if (!(sessionStorage.getItem("email")) && 
 (url_actually == url_registro) || url_actually == url_login){
     
     console.log("debe registrarse"); //deberia informarle que debe registrarse para continuar
